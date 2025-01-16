514ff2d9a8e762f9b4116386e77107dd1921bc39
AmanBenare19 <158426409+AmanBenare19@users.noreply.github.com>
2025-01-16 00:56:40 -0500

    Add files via upload

---
+++ Questions_netflix.sql
@@ -0,0 +1,15 @@
+1. Determine the count of Movies versus TV Shows.
+2. Identify the most frequent rating assigned to movies and TV shows.
+3. Display all movies released in a given year (e.g., 2018).
+4. Find the top 5 countries with the highest volume of content on Netflix.
+5. Locate the longest movie available on Netflix.
+6. Retrieve content added to Netflix within the last 5 years.
+7. List all movies and TV shows directed by 'Kirsten Johnson.'
+8. Identify TV shows with more than 5 seasons.
+9. Count the total number of content items categorized by genre.
+10. Calculate the average yearly content release in Canada on Netflix and return the top 5 years with the highest averages.
+11. List all movies categorized as documentaries.
+12. Identify all content that lacks a director.
+13. Count the number of movies featuring actor 'Salman Khan' over the past 10 years.
+14. Find the top 10 actors who have appeared in the most movies produced in Canada.
+15. Classify content based on the presence of keywords 'kill' and 'violence' in descriptions, labeling them as 'Bad' or 'Good,' and count the number in each category.
